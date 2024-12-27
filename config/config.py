f928f11c5eefbff4203ea2824f28c2f980c1c2d3
qx3kk <romanchuk.up@gmail.com>
2024-12-28 01:11:23 +0200

    chore: move .env file to the main dir

--- config/config.py
+++ config/config.py
@@ -2,10 +2,10 @@ import os
 
 from dotenv import find_dotenv, load_dotenv
 
-load_dotenv(find_dotenv())
+load_dotenv(find_dotenv(filename='.env'))
 
 DATABASE_NAME: str = os.getenv('DATABASE_NAME')
 DATABASE_USER: str = os.getenv('DATABASE_USER')
 DATABASE_PASSWORD: str = os.getenv('DATABASE_PASSWORD')
-DATABASE_HOST: str = os.getenv('DATABASE_HOST')
+DATABASE_HOST: str = os.getenv('DATABASE_HOST', 'localhost')
 DATABASE_PORT: str = os.getenv('DATABASE_PORT')
